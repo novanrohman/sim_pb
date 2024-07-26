3955a14325a5dfc009d92664277cd5084bc037cd
nova_rohman <novanrohman@gmail.com>
2024-07-26 14:36:30 +0700

    model (tahun ajaran)

--- database/migrations/2024_06_27_160311_create_tahun_ajarans_table.php
+++ database/migrations/2024_06_27_160311_create_tahun_ajarans_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('tahun_ajarans', function (Blueprint $table) {
             $table->id();
+            $table->string('tahun', );
+            $table->enum('status', ['aktif','tidak aktif']);
             $table->timestamps();
         });
     }
